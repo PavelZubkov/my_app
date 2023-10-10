baec7c1ac415080ba8c5bfdaf9dc398e0e255af3
PavelZubkov <PavelZubkov@users.noreply.github.com>
2023-10-10 07:07:22 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.mjs
+++ web.mjs
@@ -3780,6 +3780,15 @@ var $;
         dom_name() {
             return "a";
         }
+        uri_off() {
+            return "";
+        }
+        uri_native() {
+            return null;
+        }
+        external() {
+            return false;
+        }
         attr() {
             return {
                 ...super.attr(),
