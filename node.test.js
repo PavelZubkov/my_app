b65b6cbd66046ca6fd2c3a1132aa5d98eaf2ed62
PavelZubkov <PavelZubkov@users.noreply.github.com>
2023-12-13 07:07:25 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -5638,8 +5638,8 @@ var $;
             return [];
         }
         Body_content() {
-            const obj = new this.$.$mol_list();
-            obj.rows = () => this.body();
+            const obj = new this.$.$mol_view();
+            obj.sub = () => this.body();
             return obj;
         }
         body_content() {
@@ -5764,6 +5764,9 @@ var $;
             },
             Body_content: {
                 padding: $mol_gap.block,
+                flex: {
+                    direction: 'column',
+                },
                 justify: {
                     self: 'stretch',
                 },
--- web.mjs
+++ web.mjs
@@ -5085,8 +5085,8 @@ var $;
             return [];
         }
         Body_content() {
-            const obj = new this.$.$mol_list();
-            obj.rows = () => this.body();
+            const obj = new this.$.$mol_view();
+            obj.sub = () => this.body();
             return obj;
         }
         body_content() {
@@ -5211,6 +5211,9 @@ var $;
             },
             Body_content: {
                 padding: $mol_gap.block,
+                flex: {
+                    direction: 'column',
+                },
                 justify: {
                     self: 'stretch',
                 },
